c56723ea6f63293776dc365de2ca80f8347987c6
Paolo Barone <paobar@amazon.com>
2024-05-23 13:57:08 -0400

    Separate captures for microphone and system audio channels

--- Examples/DesktopApp/TranscribeApp.Designer.cs
+++ Examples/DesktopApp/TranscribeApp.Designer.cs
@@ -29,55 +29,122 @@
         private void InitializeComponent()
         {
             rtbTranscription = new RichTextBox();
-            btnStartTranscription = new Button();
+            btnRecordMike = new Button();
             lblInputLanguage = new Label();
             cbxLanguages = new ComboBox();
+            btnRecordSpeakers = new Button();
+            rtbTranscriptionMike = new RichTextBox();
+            label1 = new Label();
+            cbxInputDevices = new ComboBox();
+            cbxSpkrLanguages = new ComboBox();
+            label2 = new Label();
             SuspendLayout();
             // 
             // rtbTranscription
             // 
-            rtbTranscription.Location = new Point(15, 91);
+            rtbTranscription.Location = new Point(411, 167);
             rtbTranscription.Name = "rtbTranscription";
-            rtbTranscription.Size = new Size(773, 347);
+            rtbTranscription.Size = new Size(377, 271);
             rtbTranscription.TabIndex = 0;
             rtbTranscription.Text = "";
             // 
-            // btnStartTranscription
+            // btnRecordMike
             // 
-            btnStartTranscription.Location = new Point(14, 19);
-            btnStartTranscription.Name = "btnStartTranscription";
-            btnStartTranscription.Size = new Size(137, 45);
-            btnStartTranscription.TabIndex = 1;
-            btnStartTranscription.Text = "Start Transcription";
-            btnStartTranscription.UseVisualStyleBackColor = true;
+            btnRecordMike.Location = new Point(14, 19);
+            btnRecordMike.Name = "btnRecordMike";
+            btnRecordMike.Size = new Size(137, 45);
+            btnRecordMike.TabIndex = 1;
+            btnRecordMike.Text = "Start Mic Transcription";
+            btnRecordMike.UseVisualStyleBackColor = true;
             // 
             // lblInputLanguage
             // 
             lblInputLanguage.AutoSize = true;
-            lblInputLanguage.Location = new Point(205, 34);
+            lblInputLanguage.Location = new Point(14, 77);
             lblInputLanguage.Name = "lblInputLanguage";
-            lblInputLanguage.Size = new Size(90, 15);
+            lblInputLanguage.Size = new Size(82, 15);
             lblInputLanguage.TabIndex = 2;
-            lblInputLanguage.Text = "Input Language";
+            lblInputLanguage.Text = "Mic Language";
             // 
             // cbxLanguages
             // 
             cbxLanguages.DropDownStyle = ComboBoxStyle.DropDownList;
             cbxLanguages.FormattingEnabled = true;
-            cbxLanguages.Location = new Point(312, 31);
+            cbxLanguages.Location = new Point(110, 74);
             cbxLanguages.Name = "cbxLanguages";
-            cbxLanguages.Size = new Size(242, 23);
+            cbxLanguages.Size = new Size(281, 23);
             cbxLanguages.TabIndex = 3;
             cbxLanguages.SelectedIndexChanged += cbxLanguages_SelectedIndexChanged;
+            // 
+            // btnRecordSpeakers
+            // 
+            btnRecordSpeakers.Location = new Point(651, 19);
+            btnRecordSpeakers.Name = "btnRecordSpeakers";
+            btnRecordSpeakers.Size = new Size(137, 45);
+            btnRecordSpeakers.TabIndex = 4;
+            btnRecordSpeakers.Text = "Start Speakers Transcription";
+            btnRecordSpeakers.UseVisualStyleBackColor = true;
+            // 
+            // rtbTranscriptionMike
+            // 
+            rtbTranscriptionMike.Location = new Point(14, 167);
+            rtbTranscriptionMike.Name = "rtbTranscriptionMike";
+            rtbTranscriptionMike.Size = new Size(377, 271);
+            rtbTranscriptionMike.TabIndex = 5;
+            rtbTranscriptionMike.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Input Device";
+            // 
+            // cbxInputDevices
+            // 
+            cbxInputDevices.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxInputDevices.FormattingEnabled = true;
+            cbxInputDevices.Location = new Point(110, 117);
+            cbxInputDevices.Name = "cbxInputDevices";
+            cbxInputDevices.Size = new Size(281, 23);
+            cbxInputDevices.TabIndex = 7;
+            // 
+            // cbxSpkrLanguages
+            // 
+            cbxSpkrLanguages.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxSpkrLanguages.FormattingEnabled = true;
+            cbxSpkrLanguages.Location = new Point(507, 77);
+            cbxSpkrLanguages.Name = "cbxSpkrLanguages";
+            cbxSpkrLanguages.Size = new Size(281, 23);
+            cbxSpkrLanguages.TabIndex = 9;
+            cbxSpkrLanguages.SelectedIndexChanged += cbxSpkrLanguages_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(411, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Speakers Language";
+            
             // 
             // TranscribeApp
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(cbxSpkrLanguages);
+            Controls.Add(label2);
+            Controls.Add(cbxInputDevices);
+            Controls.Add(label1);
+            Controls.Add(rtbTranscriptionMike);
+            Controls.Add(btnRecordSpeakers);
             Controls.Add(cbxLanguages);
             Controls.Add(lblInputLanguage);
-            Controls.Add(btnStartTranscription);
+            Controls.Add(btnRecordMike);
             Controls.Add(rtbTranscription);
             Name = "TranscribeApp";
             Text = "Transcribe";
@@ -89,8 +156,14 @@
         #endregion
 
         private RichTextBox rtbTranscription;
-        private Button btnStartTranscription;
+        private Button btnRecordMike;
         private Label lblInputLanguage;
         private ComboBox cbxLanguages;
+        private Button btnRecordSpeakers;
+        private RichTextBox rtbTranscriptionMike;
+        private Label label1;
+        private ComboBox cbxInputDevices;
+        private ComboBox cbxSpkrLanguages;
+        private Label label2;
     }
 }
--- Examples/DesktopApp/TranscribeApp.cs
+++ Examples/DesktopApp/TranscribeApp.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.IO;
 using System.Reflection.Metadata;
@@ -14,6 +15,7 @@ using Amazon.TranscribeStreamingService.Models;
 
 using NAudio;
 using NAudio.Wave;
+using static System.Net.Mime.MediaTypeNames;
 
 
 
@@ -24,7 +26,13 @@ namespace Amazon.TranscribeStreamingService.Example
     {
 
         // Audio recording components
-        private WaveInEvent? _waveIn;
+        private WaveInEvent? _microphoneCapture;
+        private WasapiLoopbackCapture? _loopbackCapture;
+
+        private BufferedWaveProvider _micBuffer;
+        private BufferedWaveProvider _loopbackBuffer;
+        private MixingWaveProvider32 _mixer;
+
         private MemoryStream? _memoryStream;
         private Task? _sendAudioTask;
         private Task? _receiveTranscriptionTask;
@@ -37,10 +45,18 @@ namespace Amazon.TranscribeStreamingService.Example
         private static string result = "";
 
         // Amazon Transcribe Streaming Client
-        private static AmazonTranscribeStreamingClient client;
+        private static AmazonTranscribeStreamingClient speakersClient;
+        private static AmazonTranscribeStreamingClient clientMic;
 
         // Default language code
         private string _languageCode = "en-US";
+        private string _speakersLanguageCode = "en-US";
+
+        // selected Wave In Device
+        private int _selectedWaveInDevice = 0;
+
+        // selected Wave Out Device
+
 
         // Method to get supported languages for transcription
         // Currently language codes are hardcoded
@@ -48,30 +64,42 @@ namespace Amazon.TranscribeStreamingService.Example
         {
             var languages = new Dictionary<string, string>
         {
-            { "en-US", "English (US)" },
-            { "en-GB", "English (UK)" },
-            { "en-AU", "English (AU)" },
-            { "en-IN", "English (IN)" },
-            { "es-US", "Spanish (US)" },
-            { "es-ES", "Spanish (ES)" },
-            { "fr-FR", "French (FR)" },
-            { "fr-CA", "French (CA)" },
-            { "de-DE", "German (DE)" },
-            { "it-IT", "Italian (IT)" },
-            { "pt-BR", "Portuguese (BR)" },
-            { "pt-PT", "Portuguese (PT)" },
-            { "nl-NL", "Dutch (NL)" },
-            { "hi-IN", "Hindi (IN)" },
-            { "ko-KR", "Korean (KR)" },
-            { "ja-JP", "Japanese (JP)" },
-            { "zh-CN", "Mandarin Chinese (Mainland)" },
-            { "ar-SA", "Arabic (SA)" },
-            { "ru-RU", "Russian (RU)" },
-            { "tr-TR", "Turkish (TR)" }
+            {"en-US", "English, US"},
+            {"en-GB", "English, British"},
+            {"en-AU", "English, Australian"},
+            {"zh-CN", "Chinese, Simplified"},
+            {"fr-FR", "French"},
+            {"fr-CA", "French, Canadian"},
+            {"de-DE", "German"},
+            {"hi-IN", "Hindi, Indian"},
+            {"it-IT", "Italian"},
+            {"ja-JP", "Japanese"},
+            {"ko-KR", "Korean"},
+            {"pt-BR", "Portuguese, Brazilian"},
+            {"es-US", "Spanish, US"},
+            {"th-TH", "Thai"}
+
         };
 
 
             return await Task.FromResult(languages);
+        }
+
+        private void PopulateWaveInDevices(ComboBox comboBox)
+        {
+            comboBox.Items.Clear();
+
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                var deviceInfo = WaveIn.GetCapabilities(i);
+                comboBox.Items.Add($"{i}: {deviceInfo.ProductName}");
+            }
+
+            if (comboBox.Items.Count > 0)
+            {
+                comboBox.SelectedIndex = 0; // Select the first device by default
+                _selectedWaveInDevice = 0;
+            }
         }
 
         // Method to populate language combo box
@@ -81,11 +109,16 @@ namespace Amazon.TranscribeStreamingService.Example
             try
             {
                 var languages = await GetSupportedLanguagesAsync();
+                
                 cbxLanguages.DataSource = new BindingSource(languages, null);
                 cbxLanguages.DisplayMember = "Value";
                 cbxLanguages.ValueMember = "Key";
-
                 _languageCode = cbxLanguages.SelectedValue.ToString();
+
+                cbxSpkrLanguages.DataSource = new BindingSource(languages, null);
+                cbxSpkrLanguages.DisplayMember = "Value";
+                cbxSpkrLanguages.ValueMember = "Key";
+                _speakersLanguageCode = cbxSpkrLanguages.SelectedValue.ToString();
             }
 
             catch (Exception ex)
@@ -102,31 +135,38 @@ namespace Amazon.TranscribeStreamingService.Example
             InitializeComponent();
 
             // Event handler for the start transcription button
-            btnStartTranscription.Click += async (sender, e) => await btnStartTranscription_Click();
+            btnRecordMike.Click += async (sender, e) => await btnRecordMike_Click();
+            btnRecordSpeakers.Click += async (sender, e) => await btnRecordSpeakers_Click();
 
             // Populate the language combo box
             PopulateLanguageComboBox();
+
+            // Populate the wave in device combo box
+            PopulateWaveInDevices(cbxInputDevices);
         }
 
         // Method to update the rich text box with the current transcription text
-        void updateGUI(string txt)
+        void updateGUI(string txt, RichTextBox rtb)
         {
-            rtbTranscription.Text = txt;
-;
-            rtbTranscription.SelectionStart = rtbTranscription.TextLength;
-            rtbTranscription.SelectionLength = 0;
-            rtbTranscription.ScrollToCaret();
+            // Append the new text to the existing text)
+            rtb.Text = txt;
+
+            rtb.SelectionStart = rtbTranscription.TextLength;
+            rtb.SelectionLength = 0;
+            rtb.ScrollToCaret();
         }
 
         // Event handler for receiving transcription events
-        private void TranscriptEvent(object? sender, TranscriptEvent transcriptEvent)
+        private void TranscriptEventMike(object? sender, TranscriptEvent transcriptEvent)
         {
             result = transcriptEvent.Transcript?.Results?.FirstOrDefault()?.Alternatives?.FirstOrDefault()?.Transcript;
             var ispartial = transcriptEvent.Transcript?.Results?.FirstOrDefault()?.IsPartial.ToString();
 
-            
+
+
             if (result != null)
             {
+                Debug.WriteLine(result.Length.ToString() + " - " + ispartial.ToString() + " - " + result);
                 if (ispartial == "False")
                 {
 
@@ -146,15 +186,65 @@ namespace Amazon.TranscribeStreamingService.Example
                 else
                 {
                     //string currenttext = TranscribedText + "\n" + result;
-                    
-                    
+
+
                     if (InvokeRequired)
                     {
-                        Invoke(new MethodInvoker(() => updateGUI(result)));
+                        Invoke(new MethodInvoker(() => updateGUI(result, rtbTranscriptionMike)));
                     }
                     else
                     {
-                        updateGUI(result);
+                        updateGUI(result, rtbTranscriptionMike);
+                    }
+
+                    Debug.WriteLine("+");
+                    return;
+                }
+            }
+            
+            //Console.WriteLine("TEST: " + result);
+        }
+
+        private void TranscriptSpeakersEvent(object? sender, TranscriptEvent transcriptEvent)
+        {
+            result = transcriptEvent.Transcript?.Results?.FirstOrDefault()?.Alternatives?.FirstOrDefault()?.Transcript;
+            var ispartial = transcriptEvent.Transcript?.Results?.FirstOrDefault()?.IsPartial.ToString();
+
+            if (result != null)
+            {
+
+                var starttime = transcriptEvent.Transcript.Results.FirstOrDefault().StartTime;
+                var alternatives = transcriptEvent.Transcript?.Results?.FirstOrDefault().Alternatives.Count;
+
+                Debug.WriteLine(result.Length.ToString() + " - " + ispartial.ToString() + " - " + starttime.ToString() + " - " + alternatives.ToString()+ " - " + result);
+                if (ispartial == "False")
+                {
+
+                    TranscribedText += result + "\n";
+
+
+                    //Debug.WriteLine(TranscribedText);
+
+                    //if (InvokeRequired)
+                    //{
+                    //   Invoke(new MethodInvoker(() => updateGUI(TranscribedText)));
+                    //} else
+                    //{
+                    //   updateGUI(TranscribedText);
+                    //}
+                }
+                else
+                {
+                    //string currenttext = TranscribedText + "\n" + result;
+
+
+                    if (InvokeRequired)
+                    {
+                        Invoke(new MethodInvoker(() => updateGUI(result, rtbTranscription)));
+                    }
+                    else
+                    {
+                        updateGUI(result, rtbTranscription);
                     }
 
                     Debug.WriteLine("+");
@@ -176,18 +266,83 @@ namespace Amazon.TranscribeStreamingService.Example
         {
 
         }
-        
+
+
+        private async Task btnRecordSpeakers_Click()
+        {
+            if (btnRecordSpeakers.Text == "Stop Speakers Transcription")
+            {
+
+                // Stop the recording and streaming
+                _loopbackCapture.StopRecording();
+                //_microphoneCapture.StopRecording();
+
+                await speakersClient.StopStreaming();
+                btnRecordSpeakers.Text = "Start Speakers Transcription";
+                cbxSpkrLanguages.Enabled = true;
+                
+                return;
+
+            }
+
+            _loopbackCapture = new WasapiLoopbackCapture
+            {
+                WaveFormat = new WaveFormat(16000, 16, 2)
+            };
+
+            var waveFormat = _loopbackCapture.WaveFormat;
+            int sampleRate = waveFormat.SampleRate;
+            int channels = waveFormat.Channels;
+
+            var region = "us-east-1";
+
+            Config config = new Config("pcm", sampleRate.ToString(), _speakersLanguageCode);
+            config.EnableChannelIdentification = "true";
+            config.NumberOfChannels = "2";
+            config.EnablePartialResultsStabilization = "true";
+
+            speakersClient = new AmazonTranscribeStreamingClient(region, config, null);
+
+            speakersClient.TranscriptEvent += TranscriptSpeakersEvent;
+            speakersClient.TranscriptException += TranscribeException;
+
+            btnRecordSpeakers.Text = "Stop Transcription";
+            cbxSpkrLanguages.Enabled = false;
+            await speakersClient.StartStreaming();
+
+            int chunkSizeTime = 100; // 100 millisecond chunks
+            int chunkSize = 8000 * 2 / (1000 / chunkSizeTime); // calculate the chunk size
+
+            _memoryStream = new MemoryStream();
+
+            _loopbackCapture.DataAvailable += (s, a) => _memoryStream.Write(a.Buffer, 0, a.BytesRecorded);
+
+            _loopbackCapture.RecordingStopped += (s, a) => _memoryStream.Close();
+
+            _loopbackCapture.StartRecording();
+
+            _loopbackCapture.DataAvailable += async (s, a) =>
+            {
+                speakersClient.StreamBuffer(a.Buffer);
+                await _memoryStream.FlushAsync();
+            };
+
+
+
+        }
+
+
         // Event handler for start/stop transcription button click
-        private async Task btnStartTranscription_Click()
+        private async Task btnRecordMike_Click()
         {
 
-            if (btnStartTranscription.Text == "Stop Transcription")
+            if (btnRecordMike.Text == "Stop Transcription")
             {
-                
+
                 // Stop the recording and streaming
-                _waveIn.StopRecording();
-                await client.StopStreaming();
-                btnStartTranscription.Text = "Start Transcription";
+                _microphoneCapture.StopRecording();
+                await clientMic.StopStreaming();
+                btnRecordMike.Text = "Start Transcription";
                 cbxLanguages.Enabled = true;
                 return;
 
@@ -199,50 +354,60 @@ namespace Amazon.TranscribeStreamingService.Example
             config.EnableChannelIdentification = "true";
             config.NumberOfChannels = "2";
             config.EnablePartialResultsStabilization = "true";
-            
-            client = new AmazonTranscribeStreamingClient(region, config, null);
 
-            client.TranscriptEvent += TranscriptEvent;
-            client.TranscriptException += TranscribeException;
+            clientMic = new AmazonTranscribeStreamingClient(region, config, null);
 
-            btnStartTranscription.Text = "Stop Transcription";
+            clientMic.TranscriptEvent += TranscriptEventMike;
+            clientMic.TranscriptException += TranscribeException;
+
+            btnRecordMike.Text = "Stop Transcription";
             cbxLanguages.Enabled = false;
-            await client.StartStreaming();
+            await clientMic.StartStreaming();
 
             int chunkSizeTime = 100; // 100 millisecond chunks
             int chunkSize = 8000 * 2 / (1000 / chunkSizeTime); // calculate the chunk size
 
-            _waveIn = new WaveInEvent
+            _microphoneCapture = new WaveInEvent
             {
                 WaveFormat = new WaveFormat(16000, 1), // 16kHz, mono
-                DeviceNumber = 1
+                DeviceNumber = _selectedWaveInDevice
             };
 
             _memoryStream = new MemoryStream();
-            _waveIn.DataAvailable += (s, a) => _memoryStream.Write(a.Buffer, 0, a.BytesRecorded);
-            _waveIn.RecordingStopped += (s, a) => _memoryStream.Close();
+            _microphoneCapture.DataAvailable += (s, a) => _memoryStream.Write(a.Buffer, 0, a.BytesRecorded);
+            _microphoneCapture.RecordingStopped += (s, a) => _memoryStream.Close();
 
-            _waveIn.StartRecording();
+            _microphoneCapture.StartRecording();
 
             //TranscribedText += "\n Transcription started.\n";
             //TranscribedText += _languageCode + "\n";
 
-            _waveIn.DataAvailable += async (s, a) =>
+            _microphoneCapture.DataAvailable += async (s, a) =>
             {
 
-                client.StreamBuffer(a.Buffer);
+                clientMic.StreamBuffer(a.Buffer);
 
                 await _memoryStream.FlushAsync();
             };
 
 
         }
-        
+
         // Event handler for language combo box selection change
         private void cbxLanguages_SelectedIndexChanged(object sender, EventArgs e)
         {
             _languageCode = cbxLanguages.SelectedValue.ToString();
         }
 
+
+        private void cbxSpkrLanguages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _speakersLanguageCode = cbxSpkrLanguages.SelectedValue.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
